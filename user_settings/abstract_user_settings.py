d8313ec4443cfd47c0ea6a0f81730edbcc06753c
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2022-01-04 12:07:13 +0000

    Finish user settings interface

---
+++ user_settings/abstract_user_settings.py
@@ -0,0 +1,213 @@
+from abc import abstractmethod, ABCMeta
+from enum import Enum
+from typing import List
+
+
+class ClientSuffixEnum(Enum):
+    PLATFORM = "pl"
+    NETSHIELD = "f1"
+    NETSHIELD_ADS_TRACKING = "f2"
+    NETSHIELD_NA = "f3"
+    RANDOMAZIED_NAT = "nr"
+
+
+class NetshieldEnum(Enum):
+    DISABLE = "f0"
+    MALWARE = ClientSuffixEnum.NETSHIELD.value
+    ADS_MALWARE = ClientSuffixEnum.NETSHIELD_ADS_TRACKING.value
+
+
+class KillswitchEnum(Enum):
+    DISABLE = 0
+    PERMANENT = 1
+    ENABLE = 2
+
+
+class SecureCoreEnum(Enum):
+    DISABLE = 0
+    ENABLE = 1
+
+
+class ProtocolEnum(Enum):
+    OPENVPN_TCP = "tcp"
+    OPENVPN_UDP = "udp"
+    IKEV2 = "ikev2"
+    WIREGUARD = "wireguard"
+
+
+class AlternativeRoutingEnum(Enum):
+    DISABLE = 0
+    ENABLE = 1
+
+
+class DNSEnum(Enum):
+    CUSTOM = 0
+    AUTOMATIC = 1
+
+
+class VPNAcceleratorEnum(Enum):
+    DISABLE = 0
+    ENABLE = 1
+
+
+class NotificationEnum(Enum):
+    OPENED = 0
+    NOT_OPENED = 1
+    UNKNOWN = 2
+
+
+class AbstractUserSettings(ABC):
+
+    @property
+    @abstractmethod
+    def netshield(self) -> NetshieldEnum:
+        """Get netshield to specified option."""
+        pass
+
+    @netshield.setter
+    @abstractmethod
+    def netshield(self, enum_value: NetshieldEnum):
+        """Set user netshield setting.
+        
+        :param enum_value: enum constant, ie: NetshieldEnum.DISABLED
+        :type enum_value: NetshieldEnum
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def killswitch(self) -> KillswitchEnum:
+        """Get user Kill Switch setting."""
+        pass
+
+    @killswitch.setter
+    @abstractmethod
+    def killswitch(self, enum_value: KillswitchEnum):
+        """Set Kill Switch to specified option.
+        
+        :param enum_value: enum constant, ie: KillswitchEnum.PERMANENT
+        :type enum_value: KillswitchEnum
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def secure_core(self) -> SecureCoreEnum:
+        """Get Secure Core setting.
+
+        This is mostly for GUI as it might not be very
+        relevant for CLIs.
+        """
+        pass
+
+    @secure_core.setter
+    @abstractmethod
+    def secure_core(self, enum_value: SecureCoreEnum):
+        """set Secure Core setting.
+        
+        :param enum_value: enum constant, ie: SecureCoreEnum.ENABLE
+        :type enum_value: SecureCoreEnum
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def alternative_routing(self) -> AlternativeRoutingEnum:
+        """Get alternative routing setting."""
+        pass
+
+    @alternative_routing.setter
+    @abstractmethod
+    def alternative_routing(self, enum_value: AlternativeRoutingEnum):
+        """Set alternative routing setting.
+
+        :param enum_value: enum constant, ie: AlternativeRoutingEnum.ENABLE
+        :type enum_value: AlternativeRoutingEnum
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def protocol(self) -> ProtocolEnum:
+        """Get default protocol."""
+        pass
+
+    @protocol.setter
+    @abstractmethod
+    def protocol(self, enum_value: ProtocolEnum):
+        """Set default protocol setting.
+        
+        :param enum_value: enum constant, ie: ProtocolEnum.OPENVPN_TCP
+        :type enum_value: ProtocolEnum
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def dns(self) -> DNSEnum:
+        """Get user DNS setting."""
+        pass
+
+    @dns.setter
+    @abstractmethod
+    def dns(self, enum_value: DNSEnum):
+        """Set DNS setting.
+        
+        :param enum_value: enum constant, ie: DNSEnum.AUTOMATIC
+        :type enum_value: DNSEnum
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def dns_custom_ips(self) -> List[str]:
+        """Get user DNS setting."""
+        pass
+
+    @dns_custom_ips.setter
+    @abstractmethod
+    def dns_custom_ips(self, custom_ips: List[str]):
+        """Set and replace (if exists) custom DNS lis.
+        
+        :param custom_ips: a collection of IPs in str format
+        :type enum_value: List[str]
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def vpn_accelerator(self) -> VPNAcceleratorEnum:
+        """Get user VPN Accelerator setting."""
+        pass
+
+    @vpn_accelerator.setter
+    @abstractmethod
+    def vpn_accelerator(self, enum_value: VPNAcceleratorEnum):
+        """Set VPN Accelerator lis.
+        
+        :param enum_value: enum constant, ie: VPNAcceleratorEnum.ENABLE
+        :type enum_value: VPNAcceleratorEnum
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def event_notification(self) -> NotificationEnum:
+        """Get event notification setting."""
+        pass
+
+    @event_notification.setter
+    @abstractmethod
+    def event_notification(self, enum_value: NotificationEnum):
+        """Set event notification.
+        
+        :param enum_value: enum constant, ie: NotificationEnum.OPENED
+        :type enum_value: NotificationEnum
+        """
+        pass
+
+    @abstractmethod
+    def reset_to_default_configs(self) -> bool:
+        """Reset user configuration to default values."""
+        pass
+
