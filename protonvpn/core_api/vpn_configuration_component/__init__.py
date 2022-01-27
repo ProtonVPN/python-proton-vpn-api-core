a321c14ac5fe2cee462e45c258fa2972ab0afb81
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2022-01-27 09:48:06 +0000

    Remove old code

--- protonvpn/core_api/vpn_configuration_component/__init__.py
+++
@@ -1,4 +0,0 @@
-from .openvpn_config import VPNConfigurationOpenVPNTCP, VPNConfigurationOpenVPNUDP
-from .wireguard_config import VPNConfigurationWireguard
-
-__all__ = ["VPNConfigurationOpenVPNTCP", "VPNConfigurationOpenVPNUDP", "VPNConfigurationWireguard"]
