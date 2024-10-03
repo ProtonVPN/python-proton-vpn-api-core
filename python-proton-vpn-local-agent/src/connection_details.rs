89e2d0b3b35be4cbefad4deb8516c59c1ecf6347
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2024-10-03 12:33:34 +0100

    [VPNLINUX-626] Truncate device IP.

--- python-proton-vpn-local-agent/src/connection_details.rs
+++ python-proton-vpn-local-agent/src/connection_details.rs
@@ -1,13 +1,34 @@
 pub use local_agent_rs as la;
 use pyo3::prelude::{pyclass, pymethods, PyResult};
+use std::fmt;
 
 #[pyclass(get_all)]
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct ConnectionDetails {
     pub device_ip: Option<String>,
     pub device_country: Option<String>,
     pub server_ipv4: Option<String>,
     pub server_ipv6: Option<String>,
+}
+
+impl fmt::Debug for ConnectionDetails {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let truncated_device_ip = if let Some(ip) = &self.device_ip {
+            let last_dot_index: usize =
+                ip.rfind('.').expect("IP address contains no dots.") + 1;
+            let sliced_string = &ip[0..ip.len() - (ip.len() - last_dot_index)];
+            let mut truncated_string = String::from(sliced_string);
+            truncated_string.push_str("xxx");
+            truncated_string
+        } else {
+            "None".to_string()
+        };
+        write!(
+            f,
+            "ConnectionDetails {{ device_ip: {:?}, device_country: {:?}, server_ipv4: {:?}, server_ipv6: {:?} }}",
+            truncated_device_ip, self.device_country, self.server_ipv4, self.server_ipv6
+        )
+    }
 }
 
 #[pymethods]
