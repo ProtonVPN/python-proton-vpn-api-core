c975dfb9d69194c7b94782bdc2ba5e6fe41ba2be
Luke Titley <luke.titley@proton.ch>
2025-02-20 17:10:46 +0100

    [VPNLINUX-1163] Forward the rust log to python log.

--- proton/vpn/backend/linux/networkmanager/core/local_agent/__init__.py
+++ proton/vpn/backend/linux/networkmanager/core/local_agent/__init__.py
@@ -19,6 +19,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
 """
+import proton.vpn.local_agent
+import proton.vpn.logging
+
 from proton.vpn.local_agent import (  # pylint: disable=no-name-in-module, import-error
     AgentConnector, AgentConnection, Status,
     State, Reason, ReasonCode, AgentFeatures,
@@ -27,6 +30,10 @@ from proton.vpn.local_agent import (  # pylint: disable=no-name-in-module, impor
     ConnectionDetails
 )
 from .listener import AgentListener
+
+# Initialize logging for the local agent module, this forwards the rust
+# logging to the python logging module.
+proton.vpn.local_agent.init_logger(proton.vpn.logging.getLogger)
 
 __all__ = [
     "AgentConnector", "AgentConnection", "Status",
