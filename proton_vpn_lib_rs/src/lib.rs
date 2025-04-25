2f586a05ea3e23f4b13f6c05d57b6f22104f4874
Luke Titley <luke.titley@proton.ch>
2025-04-25 10:55:50 +0200

    Use 'pythonize::depythonize' to automatically convert the LogicalServers part of the response into a list of servers that can be passed to compute_loads

--- proton_vpn_lib_rs/src/lib.rs
+++ proton_vpn_lib_rs/src/lib.rs
@@ -1,4 +1,26 @@
 // -----------------------------------------------------------------------------
 // Copyright (c) 2024 Proton AG
 // -----------------------------------------------------------------------------
+pub use procure::Server;
+
 // -----------------------------------------------------------------------------
+pub struct ServerStatus
+{
+    status_id: String,
+    servers : Vec<Server>
+}
+
+impl ServerStatus {
+    pub fn new(status_id: String, servers : Vec::<Server>) -> Self
+    {
+        Self {
+            status_id,
+            servers,
+        }
+    }
+
+    pub fn status_id(&self) -> &str
+    {
+        &self.status_id
+    }
+}
--- python-proton-vpn-lib/src/lib.rs
+++ python-proton-vpn-lib/src/lib.rs
@@ -1,21 +1,45 @@
 // -----------------------------------------------------------------------------
 // Copyright (c) 2024 Proton AG
 // -----------------------------------------------------------------------------
-use pyo3::prelude::*;
+mod error;
 // -----------------------------------------------------------------------------
+use pyo3::exceptions::PyException;
+use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyList, PyString};
+// -----------------------------------------------------------------------------
+
+pyo3::create_exception!(lib, ProtonVpnLibError, PyException);
+
+const STATUS: &str = "Status";
+const LOGICAL_SERVERS: &str = "LogicalServers";
 
 fn init_logger() {
     env_logger::init();
 }
 
 #[pyclass]
-struct Fetcher{}
+struct ServerStatus(proton_vpn_lib_rs::ServerStatus);
+
+#[pymethods]
+impl ServerStatus {
+    #[new]
+    pub fn new<'py>(response: &Bound<'py, PyAny>) -> Result<Self, PyErr> {
+        Ok(Self(proton_vpn_lib_rs::ServerStatus::new(
+            response.get_item(STATUS)?.extract()?,
+            pythonize::depythonize(&(response.get_item(LOGICAL_SERVERS)?))?,
+        )))
+    }
+
+    pub fn status_id(&self) -> &str {
+        self.0.status_id()
+    }
+}
 
 #[pymodule]
 /// This is the entry point for the python module.
-fn base(m: &Bound<'_, PyModule>) -> PyResult<()> {
-
-    m.add_class::<Fetcher>()?;
+fn lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<ServerStatus>()?;
+    m.add("ProtonVpnLibError", m.py().get_type::<ProtonVpnLibError>())?;
 
     // Start the logger when the module is returned.
     init_logger();
