0e29c51a245bfed686c2c6887123dd7f98794ed6
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2025-09-24 11:53:40 +0100

    fix: [VPNLINUX-1425] remove proton-vpn-lib dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url="https://github.com/ProtonVPN/python-proton-vpn-api-core",
     install_requires=[
         "proton-core", "distro", "sentry-sdk",
-        "cryptography", "PyNaCl", "distro", "proton-vpn-lib", "fido2"
+        "cryptography", "PyNaCl", "distro", "fido2"
     ],
     extras_require={
         "development": ["pytest", "pytest-coverage", "pylint", "flake8", "pytest-asyncio", "PyYAML"]
